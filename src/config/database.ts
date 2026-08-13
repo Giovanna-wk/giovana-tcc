e40a95e9cec6af65e3eeedc8bd00d3ed777bb527
Giovanna-wk <giovana.vitoria1302@gmail.com>
2026-08-13 10:32:56 -0300

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT) || 3306,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASS!,
-    database: process.env.DB_NAME!,
-    waitForConnections: true!,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST!,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASS!,
+  database: process.env.DB_NAME!,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
